3f35260a2f9110654de3e8dedfa21f76a0cebbe5
Amy Farrell <akf@alum.mit.edu>
2017-06-13 15:28:59 -0700

    Restore license comments

--- doc-generator/doc_formatter/__init__.py
+++ doc-generator/doc_formatter/__init__.py
@@ -1,3 +1,7 @@
+# Copyright Notice:
+# Copyright 2016 Distributed Management Task Force, Inc. All rights reserved.
+# License: BSD 3-Clause License. For full text see link: https://github.com/DMTF/Redfish-Tools/LICENSE.md
+
 from .doc_formatter import DocFormatter
 from .markdown_generator import MarkdownGenerator
 from .html_generator import HtmlGenerator
