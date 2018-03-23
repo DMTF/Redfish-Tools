99e41793e7e12fd82a7d05e69f7af57109b3a136
Mike Raineri <michael.raineri@dell.com>
2018-03-23 10:09:59 -0400

    Corrected link to license in source files

--- csdl-to-json-convertor/csdl-to-json.py
+++ csdl-to-json-convertor/csdl-to-json.py
@@ -1,7 +1,7 @@
 #! /usr/bin/python3
 # Copyright Notice:
 # Copyright 2017 Distributed Management Task Force, Inc. All rights reserved.
-# License: BSD 3-Clause License. For full text see link: https://github.com/DMTF/Redfish-Tools/LICENSE.md
+# License: BSD 3-Clause License. For full text see link: https://github.com/DMTF/Redfish-Tools/blob/master/LICENSE.md
 
 """
 CSDL to JSON Schema
