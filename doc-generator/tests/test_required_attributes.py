b2ececcc1fa9080868326f6b96a5697c6b792061
Amy Farrell <afarrell@secondrise.com>
2019-03-29 15:01:59 -0700

    Add tests for "required" attribute (#188)

---
+++ doc-generator/tests/test_required_attributes.py
@@ -0,0 +1,69 @@
+# Copyright Notice:
+# Copyright 2019 Distributed Management Task Force, Inc. All rights reserved.
+# License: BSD 3-Clause License. For full text see link: https://github.com/DMTF/Redfish-Tools/blob/master/LICENSE.md
+
+"""
+File: test_required_attributes.py
+
+Brief: test(s) for correct generation of "required" indication.
+"""
+
+import os
+import copy
+from unittest.mock import patch
+import pytest
+from doc_generator import DocGenerator
+from doc_formatter import DocFormatter
+
+testcase_path = os.path.join('tests', 'samples')
+
+base_config = {
+    'expand_defs_from_non_output_schemas': False,
+    'excluded_by_match': ['@odata.count', '@odata.navigationLink'],
+    'profile_resources': {},
+    'units_translation': {},
+    'excluded_annotations_by_match': ['@odata.count', '@odata.navigationLink'],
+    'excluded_schemas': [],
+    'excluded_properties': ['@odata.id', '@odata.context', '@odata.type'],
+    'uri_replacements': {},
+
+    'profile': {},
+    'escape_chars': [],
+
+    'output_format': 'markdown',
+}
+
+@patch('urllib.request') # so we don't make HTTP requests. NB: samples should not call for outside resources.
+# The test sample is incomplete, so we will be warned of unavailable resources (odata, Resource, and more).
+@pytest.mark.filterwarnings("ignore:Unable to find data")
+@pytest.mark.filterwarnings("ignore:Unable to read")
+@pytest.mark.filterwarnings("ignore:Unable to retrieve")
+def test_required_attribute_collection(mockRequest):
+    """
+    """
+
+    config = copy.deepcopy(base_config)
+
+    input_dir = os.path.abspath(os.path.join(testcase_path, 'required_attributes', 'input'))
+
+    config['uri_to_local'] = {'redfish.dmtf.org/schemas/v1': input_dir}
+    config['local_to_uri'] = { input_dir : 'redfish.dmtf.org/schemas/v1'}
+
+    docGen = DocGenerator([ input_dir ], '/dev/null', config)
+    output = docGen.generate_docs()
+
+    # Check a couple of properties that are directly included in property_data.
+    eventData = docGen.property_data['redfish.dmtf.org/schemas/v1/Event.json']
+    eventProperties = eventData['properties']
+    assert eventProperties['Events']['prop_required'] is True
+    assert eventProperties['Id']['prop_required'] is True
+    assert eventProperties['Context']['prop_required'] is False
+
+
+    # And some properties of an object from the "definitions" section.
+    eventDefinitions = eventData['definitions']
+    eventRecordProps = eventDefinitions['EventRecord']['properties']
+    assert eventRecordProps['EventType']['prop_required'] is True
+    assert eventRecordProps['MessageId']['prop_required'] is True
+    assert eventRecordProps['Severity']['prop_required'] is False
+    assert eventRecordProps['Oem']['prop_required'] is False
