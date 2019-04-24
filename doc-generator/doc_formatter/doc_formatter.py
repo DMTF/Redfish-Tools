da13e988f748e32d84824535edcec25cc18557d7
Amy Farrell <afarrell@secondrise.com>
2019-04-24 16:12:26 -0700

    Ensure DocFormatter classes have current_uris attribute defined

--- doc-generator/doc_formatter/doc_formatter.py
+++ doc-generator/doc_formatter/doc_formatter.py
@@ -42,6 +42,7 @@ class DocFormatter:
         self.collapse_list_of_simple_type = True
         self.formatter = FormatUtils() # Non-markdown formatters will override this.
         self.layout_payloads = 'bottom' # Do payloads go at top of section or bottom?
+        self.current_uris = []
 
         # Extend config with some defaults.
         self.config['excluded_pattern_props'] = self.config.get('excluded_pattern_props', [])
