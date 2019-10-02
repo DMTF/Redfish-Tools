5ab1780ffa3d5e2b51727cb68887161dd9c3585b
Jeff Autor <jeff.autor@hpe.com>
2019-10-02 18:10:50 -0500

    Added Volt-Amps to unitsWhiteList
    Unit combination valid but not included in UCUM

--- redfish-repo-test/csdl-syntax-test.js
+++ redfish-repo-test/csdl-syntax-test.js
@@ -28,7 +28,7 @@ options.cache = new CSDL.cache(options.useLocal, options.useNetwork);
 
 /***************** White lists ******************************/
 //Units that don't exist in UCUM
-const unitsWhiteList = ['RPM'];
+const unitsWhiteList = ['RPM', 'V.A'];
 //Enumeration Member names that are non-Pascal Cased
 const NonPascalCaseEnumWhiteList = ['iSCSI', 'iQN', 'FC_WWN', 'TX_RX', 'EIA_310', 'string', 'number', 'NVDIMM_N',
                                     'NVDIMM_F', 'NVDIMM_P', 'DDR4_SDRAM', 'DDR4E_SDRAM', 'LPDDR4_SDRAM', 'DDR3_SDRAM',
