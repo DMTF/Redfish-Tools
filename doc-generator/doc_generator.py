c1dfcae03aaaea5827285370fa0e8ae32eac4efc
A.K. Farrell <afarrell@secondrise.com>
2021-08-26 12:08:10 -0700

    Doc generator: correction to content supplement parsing that prevented loading of local mockup files

--- doc-generator/doc_generator.py
+++ doc-generator/doc_generator.py
@@ -1456,6 +1456,7 @@ def parse_schema_supplement(supp_data):
     """ Vet and extend supp_data. Any "mockup" entries in supp_data should be file paths or URIs to be expanded. """
     for schema_name, data in supp_data.items():
         if 'mockup' in data:
+            import pdb; pdb.set_trace()
             mockup_location = data.get('mockup')
             mockup = None
             ml_lower = mockup_location.lower()
@@ -1470,14 +1471,14 @@ def parse_schema_supplement(supp_data):
                                           {'uri': mockup_location, 'status_code': response.status})
                 except Exception as ex:
                     warnings.warn('Unable to retrieve Mockup from URL "%(uri)s": %(ex)s' % {'uri': mockup_location, 'ex': str(ex)})
-                else:
-                    # treat it as a local file
-                    try:
-                        mockup_file = open(mockup_location, 'r', encoding="utf8")
-                        mockup = mockup_file.read()
-                    except Exception as ex:
-                        warnings.warn('Unable to open Mockup file "%(uri)s" to read: %(ex)s'
-                                              % {'uri': mockup_location, 'ex': str(ex)})
+            else:
+                # treat it as a local file
+                try:
+                    mockup_file = open(mockup_location, 'r', encoding="utf8")
+                    mockup = mockup_file.read()
+                except Exception as ex:
+                    warnings.warn('Unable to open Mockup file "%(uri)s" to read: %(ex)s'
+                                          % {'uri': mockup_location, 'ex': str(ex)})
 
             if mockup:
                 if data.get('jsonpayload'):
